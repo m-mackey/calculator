ea01cd825d9bbcf5f70058136c72c66893efc660
mmackey <megan.mackey24@gmail.com>
2024-01-10 02:18:48 -0600

    Setup basic calculator functionality
    Still need to account for dividing by 0 and rounding numbers

--- scripts.js
+++ scripts.js
@@ -11,3 +11,23 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
+
+//operate function that uses above calc functions
+
+function operate(num1, num2, operator) {
+    //add check for divide by 0 before going further?
+
+    switch (operator) {
+        case '+':
+            return add(num1, num2);
+        case '-':
+            return subtract(num1, num2);
+        case '*':
+            return multiply(num1, num2);
+        case '/':
+            return divide(num1, num2);
+        default:
+            console.log('not working???')
+            break;
+    }
+}
