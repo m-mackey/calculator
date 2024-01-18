6b24793e17fd505e719251eff953a486dbc9d28e
mmackey <megan.mackey24@gmail.com>
2024-01-18 01:52:21 -0600

    Add comma to a line, prettify code

--- scripts.js
+++ scripts.js
@@ -16,23 +16,22 @@ const divide = (num1, num2) => num1 / num2;
 //operate function that uses above calc functions
 
 function operate(num1, num2, operator) {
-
-    switch (operator) {
-        case '+':
-            return add(num1, num2);
-        case '-':
-            return subtract(num1, num2);
-        case '*':
-            return multiply(num1, num2);
-        case '/':
-            if (num2 === 0) {
-                console.log('error');
-                break;
-            } else {
-                return divide(num1, num2);
-            };
-        default:
-            console.log('not working???')
-            break;
-    }
+  switch (operator) {
+    case '+':
+      return add(num1, num2);
+    case '-':
+      return subtract(num1, num2);
+    case '*':
+      return multiply(num1, num2);
+    case '/':
+      if (num2 === 0) {
+        console.log('error');
+        break;
+      } else {
+        return divide(num1, num2);
+      }
+    default:
+      console.log('not working???');
+      break;
+  }
 }
