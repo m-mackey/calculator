094e4ccd5f0378b022db15d76b49b29d44c37b99
mmackey <megan.mackey24@gmail.com>
2024-04-08 01:26:00 -0500

    Rename testArr to operationArr
    Finally.

--- scripts.js
+++ scripts.js
@@ -81,7 +81,7 @@ clearBtn.addEventListener('click', () => {
   displayedNum = '';
   display.textContent = '';
   toggleDecimalEvent();
-  testArr.length = 0;
+  operationArr.length = 0;
 })
 
 const operatorBtns = document.querySelectorAll('.operator');
@@ -89,28 +89,28 @@ operatorBtns.forEach((operator) => {
   operator.addEventListener('click', onOperatorClick);
 })
 
-let testArr = [];
+let operationArr = [];
 
 function onOperatorClick() { //prob needs a better name, also maybe this fn is getting a little long
   
   // This first check is to see if an operator has already been entered,
   // and changes the operator if another is clicked before calculation is performed.
 
-  if (testArr.length === 2) {
+  if (operationArr.length === 2) {
     //something i could do instead is have an operator variable that gets assigned whatever operator is clicked, 
     //but only actually uses that variable when other conditions are met. maybe jsut prevent it if there's no result
     //or no calculation started at all. 
-    testArr[1] = this.textContent;
-    console.log(testArr)
+    operationArr[1] = this.textContent;
+    console.log(operationArr)
   }
 
   if (/[0-9]/.test(displayedNum)) {
     //the regex test just makes sure an empty string/string with just decimal isn't pushed, maybe there is a better way
-    testArr.push(displayedNum);
-    console.log(testArr);
-    if (testArr.length < 2) {
-      testArr.push(this.textContent);
-      console.log(testArr);
+    operationArr.push(displayedNum);
+    console.log(operationArr);
+    if (operationArr.length < 2) {
+      operationArr.push(this.textContent);
+      console.log(operationArr);
     }
     displayedNum = '';
   }
@@ -122,29 +122,29 @@ const equalBtn = document.querySelector('.equals');
 equalBtn.addEventListener('click', onEqualsClick);
 
 function onEqualsClick() {
-  if (testArr.length === 2 && /[0-9]/.test(displayedNum)) {
+  if (operationArr.length === 2 && /[0-9]/.test(displayedNum)) {
     //this conditional makes sure that the first number and operator have been entered, and that the current input is a num
     //can maybe change regex to built in method but some of those seem a little wonky, so research first
-    testArr.push(displayedNum);
-    result = runCalculation(testArr);
+    operationArr.push(displayedNum);
+    result = runCalculation(operationArr);
     displayedNum = result;
     display.textContent = displayedNum; 
 
-    testArr.length = 0; //this empties array, but we need to save result if someone wants to continue calculations, and clear only when needed. maybe make result its own variable that clears when prompted
-  } else if (testArr.length === 3){
+    operationArr.length = 0; //this empties array, but we need to save result if someone wants to continue calculations, and clear only when needed. maybe make result its own variable that clears when prompted
+  } else if (operationArr.length === 3){
     //currently if an operator is clicked before the equals with the last num in a calculation, the last num still gets pushed to the array.
     //this at least makes sure calculations can continue.
-    result = runCalculation(testArr);
+    result = runCalculation(operationArr);
     displayedNum = result;
     display.textContent = displayedNum; 
-    testArr.length = 0; 
+    operationArr.length = 0; 
   }
 }
 
-function runCalculation (testArr) {
-  const num1 = +testArr[0];  //+converts the string to a number
-  const operator = testArr[1];
-  const num2 = +testArr[2];
+function runCalculation (operationArr) {
+  const num1 = +operationArr[0];  //+converts the string to a number
+  const operator = operationArr[1];
+  const num2 = +operationArr[2];
   console.log(num1, num2, operator);
   console.log(operate(num1, num2, operator));
   return operate(num1, num2, operator);
